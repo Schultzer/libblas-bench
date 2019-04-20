5caed1353a9fd76f05a23d3ce4d7724f05c52b48
Benjamin Schultzer <benjamin@schultzer.com>
2019-04-20 11:11:31 -0700

    use pointers

--- benches/intel-mkl.rs
+++ benches/intel-mkl.rs
@@ -2,11 +2,11 @@
 extern crate criterion;
 extern crate blas;
 extern crate blas_src;
-extern crate rand;
 extern crate matrixmultiply;
-use rand::Rng;
+extern crate rand;
 use blas::*;
 use criterion::Criterion;
+use rand::Rng;
 
 fn blas_src_gemm() {
     let mut rng = rand::thread_rng();
@@ -15,7 +15,21 @@ fn blas_src_gemm() {
         big.push(rng.gen::<f64>())
     }
     criterion::black_box(unsafe {
-        dgemm(b'n', b'n', 6, 6, 6, 0.3, &big, 6, &big, 6, -1.2, &mut big.clone(), 6);
+        dgemm(
+            b'n',
+            b'n',
+            6,
+            6,
+            6,
+            0.3,
+            &big,
+            6,
+            &big,
+            6,
+            -1.2,
+            &mut big.clone(),
+            6,
+        );
     })
 }
 
@@ -26,10 +40,23 @@ fn blas_src_gemm_big() {
         big.push(rng.gen::<f64>())
     }
     criterion::black_box(unsafe {
-        dgemm(b'n', b'n', 1000, 1000, 10, 0.3, &big, 1000, &big, 1000, -1.2, &mut big.clone(), 1000);
+        dgemm(
+            b'n',
+            b'n',
+            1000,
+            1000,
+            10,
+            0.3,
+            &big,
+            1000,
+            &big,
+            1000,
+            -1.2,
+            &mut big.clone(),
+            1000,
+        );
     })
 }
-
 
 fn blas_src_axpy() {
     let mut rng = rand::thread_rng();
@@ -59,7 +86,14 @@ fn axpy() {
     for _ in 0..10 {
         big.push(rng.gen::<f64>())
     }
-    criterion::black_box(libblas::level1::axpy(10, 23.0, &big, 1, &mut big.clone(), 1));
+    criterion::black_box(libblas::level1::axpy(
+        10,
+        23.0,
+        big.as_ptr(),
+        1,
+        big.clone().as_mut_ptr(),
+        1,
+    ));
 }
 
 fn axpy_big() {
@@ -68,7 +102,14 @@ fn axpy_big() {
     for _ in 0..1_000_000 {
         big.push(rng.gen::<f64>())
     }
-    criterion::black_box(libblas::level1::axpy(1_000_000, 23.0, &big, 1, &mut big.clone(), 1));
+    criterion::black_box(libblas::level1::axpy(
+        1_000_000,
+        23.0,
+        big.as_ptr(),
+        1,
+        big.clone().as_mut_ptr(),
+        1,
+    ));
 }
 
 fn gemm() {
@@ -84,12 +125,12 @@ fn gemm() {
         6,
         6,
         0.3,
-        &big,
+        big.as_ptr(),
         6,
-        &big,
+        big.as_ptr(),
         6,
         -1.2,
-        &mut big.clone(),
+        big.clone().as_mut_ptr(),
         6,
     ));
 }
@@ -107,12 +148,12 @@ fn gemm_big() {
         1000,
         10,
         0.3,
-        &big,
+        big.as_ptr(),
         1000,
-        &big,
+        big.as_ptr(),
         1000,
         -1.2,
-        &mut big.clone(),
+        big.clone().as_mut_ptr(),
         1000,
     ));
 }
@@ -124,7 +165,22 @@ fn matrixmultiply_gemm() {
         big.push(rng.gen::<f64>())
     }
     criterion::black_box(unsafe {
-        matrixmultiply::dgemm(6, 6, 6, 0.3, big.as_ptr(), 0, 6, big.as_ptr(), 0, 6, -1.2, big.clone().as_mut_ptr(), 0, 6);
+        matrixmultiply::dgemm(
+            6,
+            6,
+            6,
+            0.3,
+            big.as_ptr(),
+            0,
+            6,
+            big.as_ptr(),
+            0,
+            6,
+            -1.2,
+            big.clone().as_mut_ptr(),
+            0,
+            6,
+        );
     })
 }
 
@@ -135,7 +191,22 @@ fn matrixmultiply_gemm_big() {
         big.push(rng.gen::<f64>())
     }
     criterion::black_box(unsafe {
-        matrixmultiply::dgemm(1000, 1000, 10, 0.3, big.as_ptr(), 0, 1000, big.as_ptr(), 0, 1000, -1.2, big.clone().as_mut_ptr(), 0, 1000);
+        matrixmultiply::dgemm(
+            1000,
+            1000,
+            10,
+            0.3,
+            big.as_ptr(),
+            0,
+            1000,
+            big.as_ptr(),
+            0,
+            1000,
+            -1.2,
+            big.clone().as_mut_ptr(),
+            0,
+            1000,
+        );
     })
 }
 
@@ -145,11 +216,17 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("libblas::level1::axpy", |b| b.iter(|| axpy()));
     c.bench_function("libblas::level1::axpy-big", |b| b.iter(|| axpy_big()));
     c.bench_function("matrixmultiply dgemm", |b| b.iter(|| matrixmultiply_gemm()));
-    c.bench_function("matrixmultiply dgemm big", |b| b.iter(|| matrixmultiply_gemm_big()));
+    c.bench_function("matrixmultiply dgemm big", |b| {
+        b.iter(|| matrixmultiply_gemm_big())
+    });
     c.bench_function("blas-src intel-mkl dgemm", |b| b.iter(|| blas_src_gemm()));
-    c.bench_function("blas-src intel-mkl dgemm big", |b| b.iter(|| blas_src_gemm_big()));
+    c.bench_function("blas-src intel-mkl dgemm big", |b| {
+        b.iter(|| blas_src_gemm_big())
+    });
     c.bench_function("blas-src intel-mkl daxpy", |b| b.iter(|| blas_src_axpy()));
-    c.bench_function("blas-src intel-mkl daxpy big", |b| { b.iter(|| blas_src_axpy_big())});
+    c.bench_function("blas-src intel-mkl daxpy big", |b| {
+        b.iter(|| blas_src_axpy_big())
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
